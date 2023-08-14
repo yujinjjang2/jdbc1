750f6acfc1197aa908ad7e7199e8c81738c8044a
yujinjjang2 <https://github.com/yujinjjang2>
2023-08-14 15:39:09 +0900

    update academy

---
+++ src/edu/kh/jdbc1/JDBCExample0.java
@@ -0,0 +1,40 @@
+package edu.kh.jdbc1;
+
+public class JDBCExample0 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		0. JDBC란 ? Java에서 DB에 접근할 수 있게 해주는 Java Programming API
+				* API = Application Programming Interface
+				
+		* API = 자바 회사에서 제공하는 기능
+		* Library = 소수집단(ex. 회사)이 Java를 이용해서 만든 기능
+		
+		* 우리는 Oracle JDBC Driver(OJDBC) 사용 -> Oracle에서 Java랑 연결하고 싶어서 만든 라이브러리
+		
+		1. OJDBC11 다운로드 순서
+		
+		OJDBC 다운로드 SITE -> https://mvnrepository.com/ 
+		
+		 1) OJDBC11 검색
+		 2) Usages 다운로드가 많은 것으로 다운받기 (21.5.0.0)
+	     3) jar(자바 아카이브) 다운로드 받기(압축 풀지 않아도 됨)
+         4) 다운로드 파일 -> tools 파일에 정리
+
+		2. OJDBC11 실행 순서
+		 
+		 1) eclipse 실행
+				경로 -> C:\workspace\3_JDBC
+				* 참고해야 할 사항 : 이클립스 작업 파일에 대한 문자셋을 UTF-8로 일치시켜야 함
+		 2) Project Explorer 창 우클릭
+         3) Properties 클릭
+         4) Java Build Path 클릭
+         5) Libraries 창 클릭
+         6) Classpath 클릭 -> 오른쪽 버튼들 활성화
+         7) Add External JARs 클릭
+         8) ojdbc11-21.5.0.0 파일 열기
+         */
+	}
+
+}
